b4710cc588124f7ee0abb73059bc7306f5dabe2c
Yunus Emre Aydemir <yunusemre.aydemir@outlook.com>
2023-08-16 15:20:10 +0300

    Initialize

---
+++ BarkodluSatışProgramı/ProgramRestore/ProgramRestore/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ProgramRestore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tDosya = new System.Windows.Forms.TextBox();
+            this.bYukle = new System.Windows.Forms.Button();
+            this.bYedekSec = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tDosya
+            // 
+            this.tDosya.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tDosya.Location = new System.Drawing.Point(12, 47);
+            this.tDosya.Name = "tDosya";
+            this.tDosya.Size = new System.Drawing.Size(367, 30);
+            this.tDosya.TabIndex = 0;
+            // 
+            // bYukle
+            // 
+            this.bYukle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bYukle.Location = new System.Drawing.Point(385, 94);
+            this.bYukle.Name = "bYukle";
+            this.bYukle.Size = new System.Drawing.Size(139, 56);
+            this.bYukle.TabIndex = 1;
+            this.bYukle.Text = "YÜKLE";
+            this.bYukle.UseVisualStyleBackColor = true;
+            this.bYukle.Click += new System.EventHandler(this.bYukle_Click);
+            // 
+            // bYedekSec
+            // 
+            this.bYedekSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bYedekSec.Location = new System.Drawing.Point(385, 41);
+            this.bYedekSec.Name = "bYedekSec";
+            this.bYedekSec.Size = new System.Drawing.Size(139, 40);
+            this.bYedekSec.TabIndex = 2;
+            this.bYedekSec.Text = "Yedek Seç";
+            this.bYedekSec.UseVisualStyleBackColor = true;
+            this.bYedekSec.Click += new System.EventHandler(this.bYedekSec_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(8, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "En son aldığınız yedeği seçiniz...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 60);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bu işlemi yaptığınız zaman mevcut\r\nveriler silinecek. Yedeğinizdeki \r\nbilgiler yü" +
+    "klenecektir.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 162);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bYedekSec);
+            this.Controls.Add(this.bYukle);
+            this.Controls.Add(this.tDosya);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Barkodlu Satış Programı Yedekten Yükleme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tDosya;
+        private System.Windows.Forms.Button bYukle;
+        private System.Windows.Forms.Button bYedekSec;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BarkodluSatışProgramı/ProgramRestore/ProgramRestore/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramRestore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bYedekSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Veri dosyasını seçiniz |*.bak"; //veri dosyası seçtirme |*.bak sadece bakları seçer 
+            ofd.ShowDialog();
+            tDosya.Text = ofd.FileName;
+        }
+
+        private void bYukle_Click(object sender, EventArgs e)
+        {
+            if (tDosya.Text != "")
+            {
+                try
+                {
+                    string strSql = @"data source=(LocalDB)\MSSQLLocalDb;attachdbfilename=|DataDirectory|\BarkodDb.mdf;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+                    Cursor.Current = Cursors.WaitCursor;
+                    string yedekyolu = tDosya.Text;
+                    Application.DoEvents();
+                    string str = Application.StartupPath + @"\BarkodDb.mdf";
+                    using (SqlConnection connection = new SqlConnection(strSql))
+                    {
+                        connection.Open();
+                        SqlCommand isle = new SqlCommand(@"USE Master; If Exists(Select * From sys.databases where name= 'BarkodDb') Drop DataBase[" + str + "];RESTORE DATABASE[" + str + "] FROM DISK=N'" + tDosya.Text + "'", connection);
+                        isle.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    MessageBox.Show("Veriler Yüklenmiştir.");
+                    Process.Start(Application.StartupPath + "\\BarkodluSatis.exe");
+                    Cursor.Current = Cursors.Default;
+                    Application.Exit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString() + "Hata");
+                }
+            }
+        }
+    }
+}
